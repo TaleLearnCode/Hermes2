1cd84a62a74e39de9c6ff23193763e1adbde59aa
Chad Green <chadgreen@chadgreen.com>
2024-05-28 20:51:46 -0400

    Adding initial database

---
+++ src/ReferenceServices/ReferenceServices.Database/Scripts/Script.PostDeployment.sql
@@ -0,0 +1,6 @@
+﻿:r .\Populate\Populate_Language.sql
+:r .\Populate\Populate_WorldRegion.sql
+:r .\Populate\Populate_Country.sql
+:r .\Populate\Populate_CountryDivision.sql
+:r .\Populate\Populate_TimeZone.sql
+:r .\Populate\Populate_Language.sql
